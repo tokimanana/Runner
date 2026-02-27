60a6ea1285f2e970b9ff20609c54466eb7b7fd24
tokimanana <tokimananasarobidy@gmail.com>
2026-02-27 21:37:45 +0300

    feat: created files

--- scripts/generate.mjs
+++ scripts/generate.mjs
@@ -1,20 +1,19 @@
 const [type, name] = process.argv.slice(2);
 
 const map = {
-  s: { generator: 'service',     suffix: '.service' },
-  c: { generator: 'component',   suffix: '.component' },
-  g: { generator: 'guard',       suffix: '.guard'   },
+  s: { generator: 'service', suffix: '.service' },
+  c: { generator: 'component', suffix: '.component' },
+  g: { generator: 'guard', suffix: '.guard' },
   i: { generator: 'interceptor', suffix: '.interceptor' },
-  p: { generator: 'pipe',        suffix: '.pipe'    },
+  p: { generator: 'pipe', suffix: '.pipe' },
 };
 
 const { generator, suffix } = map[type];
 const { execSync } = await import('child_process');
 
-execSync(
-  `nx g @nx/angular:${generator} ${name}${suffix} --project=frontend`,
-  { stdio: 'inherit' }
-);
+execSync(`nx g @nx/angular:${generator} ${name}${suffix} --project=frontend`, {
+  stdio: 'inherit',
+});
 
 //npm run g s core/auth/auth
 // npm run g c features/home/home
