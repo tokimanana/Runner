9a62492578021b19e293eb4f7f7910985fe067b0
tokimanana <tokimananasarobidy@gmail.com>
2026-03-04 14:06:16 +0300

    feat(login): created login form template and logic

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -3,10 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  title = 'frontend';
-}
+export class AppComponent {}
--- apps/frontend/src/app/features/auth/login/login.component.ts
+++ apps/frontend/src/app/features/auth/login/login.component.ts
@@ -1,9 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { AuthActions } from '@/app/core/auth/store/auth.actions';
+import { Observable } from 'rxjs';
+import {
+  selectAuthError,
+  selectIsLoading,
+} from '@/app/core/auth/store/auth.selectors';
+import { InputText } from 'primeng/inputtext';
+import { Message } from 'primeng/message';
+import { Password } from 'primeng/password';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    CommonModule,
+    InputText,
+    Message,
+    Password,
+    Button,
+    ReactiveFormsModule,
+    AsyncPipe,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  private readonly fb = inject(FormBuilder);
+  private readonly store = inject(Store);
+
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  isLoading$: Observable<boolean> = this.store.select(selectIsLoading);
+  error$: Observable<string | null> = this.store.select(selectAuthError);
+
+  onLogin(): void {
+    this.loginForm.markAllAsTouched();
+
+    if (this.loginForm.valid) {
+      this.store.dispatch(
+        AuthActions.login({
+          email: this.email?.value,
+          password: this.password?.value,
+        })
+      );
+    }
+  }
+}
