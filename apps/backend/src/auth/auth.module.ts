b2ac7244a48ef40fb739429acf92cd0e9e924c4c
tokimanana <tokimananasarobidy@gmail.com>
2026-03-25 17:44:13 +0300

    fix: format file

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -1,21 +1,24 @@
+import { JwtStrategy } from '@backend/auth/jwt.strategy';
+import { UsersModule } from '@backend/users/users.module';
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import {UsersModule} from "@backend/users/users.module";
-import {PassportModule} from "@nestjs/passport";
-import {JwtModule} from "@nestjs/jwt";
-import {JwtStrategy} from "@backend/auth/jwt.strategy";
+import type { StringValue } from 'ms';
 
 @Module({
   imports: [
-      UsersModule,
-      PassportModule,
-      JwtModule.register({
-          secret: process.env.JWT_SECRET,
-          signOptions: {expiresIn: process.env.JWT_EXPIRATION ?? '1h'},
-          })
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: (process.env.JWT_EXPIRATION ?? '1d') as StringValue,
+      },
+    }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy]
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
