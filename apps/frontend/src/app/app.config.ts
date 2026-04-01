ad2effbc57939b18a36212da3ae727fdb90fd020
tokimanana <tokimananasarobidy@gmail.com>
2026-04-01 23:54:02 +0300

    feat(theme): update to new preset

--- apps/frontend/src/app/app.config.ts
+++ apps/frontend/src/app/app.config.ts
@@ -10,6 +10,7 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideEffects } from '@ngrx/effects';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { definePreset } from '@primeng/themes';
 import Aura from '@primeng/themes/aura';
 import { providePrimeNG } from 'primeng/config';
 
@@ -19,6 +20,24 @@ import { authInterceptor } from './core/auth/interceptors/auth.interceptor';
 import { refreshInterceptor } from './core/auth/interceptors/refresh.interceptor';
 import { AuthEffects } from './core/auth/store/auth.effects';
 import { authReducer } from './core/auth/store/auth.reducer';
+
+const RunnerPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{stone.50}',
+      100: '{stone.100}',
+      200: '{stone.200}',
+      300: '{stone.300}',
+      400: '{stone.400}',
+      500: '{stone.500}',
+      600: '{stone.600}',
+      700: '{stone.700}',
+      800: '{stone.800}',
+      900: '{stone.900}',
+      950: '{stone.950}',
+    },
+  },
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -35,7 +54,7 @@ export const appConfig: ApplicationConfig = {
     }),
     providePrimeNG({
       theme: {
-        preset: Aura,
+        preset: RunnerPreset,
         options: {
           darkModeSelector: '.dark-mode',
         },
