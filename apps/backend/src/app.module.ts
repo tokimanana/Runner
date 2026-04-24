f23f2fb377bbe47a5537111e9825e2e2d06ae052
tokimanana <tokimananasarobidy@gmail.com>
2026-04-24 23:55:25 +0300

    feat(seasons): scaffold seasons module, controller and service

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { HotelsModule } from './hotels/hotels.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { SeasonsModule } from './seasons/seasons.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule, HotelsModule],
+  imports: [PrismaModule, AuthModule, UsersModule, HotelsModule, SeasonsModule],
   controllers: [AppController],
   providers: [AppService],
 })
