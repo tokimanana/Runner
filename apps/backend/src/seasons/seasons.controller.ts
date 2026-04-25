d6355631d8fb7c69e1b70c1c1eeb676a7ef24f35
tokimanana <tokimananasarobidy@gmail.com>
2026-04-25 21:52:29 +0300

    feat(seasons): add seasons endpoints

--- apps/backend/src/seasons/seasons.controller.ts
+++ apps/backend/src/seasons/seasons.controller.ts
@@ -1,4 +1,85 @@
-import { Controller } from '@nestjs/common';
+import { Roles } from '@backend/auth/decorators/roles.decorator';
+import { JwtAuthGuard } from '@backend/auth/guards/jwt-auth.guard';
+import { RolesGuard } from '@backend/auth/guards/roles.guard';
+import { RequestWithUser } from '@backend/auth/types/jwt-user.type';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { UserRole } from '@prisma/client';
+import { CreateSeasonDto } from './dto/create-season.dto';
+import { UpdateSeasonDto } from './dto/update-season.dto';
+import { SeasonsService } from './seasons.service';
 
 @Controller('seasons')
-export class SeasonsController {}
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(UserRole.ADMIN, UserRole.MANAGER)
+export class SeasonsController {
+  constructor(private readonly seasonService: SeasonsService) {}
+
+  @Post()
+  create(
+    @Body() createSeasonDto: CreateSeasonDto,
+    @Req() req: RequestWithUser,
+  ) {
+    const tourOperatorId = req.user.tourOperatorId;
+    return this.seasonService.create(createSeasonDto, tourOperatorId);
+  }
+
+  @Get()
+  findAll(
+    @Req() req: RequestWithUser,
+    @Query()
+    {
+      startDate,
+      endDate,
+      limit,
+      offset,
+    }: {
+      startDate?: string;
+      endDate?: string;
+      limit?: number;
+      offset?: number;
+    },
+  ) {
+    const tourOperatorId = req.user.tourOperatorId;
+    return this.seasonService.findAll(tourOperatorId, {
+      startDate: startDate ? new Date(startDate) : undefined,
+      endDate: endDate ? new Date(endDate) : undefined,
+      limit: limit ? Number(limit) : undefined,
+      offset: offset ? Number(offset) : undefined,
+    });
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string, @Req() req: RequestWithUser) {
+    const tourOperatorId = req.user.tourOperatorId;
+    return this.seasonService.findOne(id, tourOperatorId);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateSeasonDto: UpdateSeasonDto,
+    @Req() req: RequestWithUser,
+  ) {
+    const tourOperatorId = req.user.tourOperatorId;
+    return this.seasonService.update(id, updateSeasonDto, tourOperatorId);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  remove(@Param('id') id: string, @Req() req: RequestWithUser) {
+    const tourOperatorId = req.user.tourOperatorId;
+    return this.seasonService.remove(id, tourOperatorId);
+  }
+}
