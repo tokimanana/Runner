95fda4527381dbf31cb54ea27c4e02a41ecbd264
tokimanana <tokimananasarobidy@gmail.com>
2026-04-30 13:47:57 +0300

    feat(hotels): implement hotels list component with p-table and CRUD navigation

--- apps/frontend/src/app/features/management/hotels/hotels-list/hotels-list.component.ts
+++ apps/frontend/src/app/features/management/hotels/hotels-list/hotels-list.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
-
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Button } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { HotelsService } from '../hotels.service';
 @Component({
   selector: 'app-hotels-list',
-  imports: [],
+  imports: [AsyncPipe, TableModule, Button, RouterLink],
   templateUrl: './hotels-list.component.html',
   styleUrl: './hotels-list.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HotelsListComponent {}
+export class HotelsListComponent {
+  private readonly hotelsService = inject(HotelsService);
+
+  readonly hotels$ = this.hotelsService.getHotels();
+  readonly loading$ = this.hotelsService.loading$;
+
+  onDelete(id: string) {
+    console.log('Deleting', id);
+    this.hotelsService.deleteHotel(id).subscribe();
+  }
+}
