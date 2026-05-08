44182827ae2c0fba746b0c362554f2638404238f
tokimanana <tokimananasarobidy@gmail.com>
2026-05-08 16:31:57 +0300

    refactor(hotels): format file

--- apps/backend/src/hotels/repositories/prisma-hotel.repository.ts
+++ apps/backend/src/hotels/repositories/prisma-hotel.repository.ts
@@ -32,10 +32,7 @@ export class PrismaHotelRepository implements HotelRepository {
     tourOperatorId: string,
   ): Promise<HotelDetail> {
     return this.prisma.hotel.create({
-      data: {
-        ...data,
-        tourOperatorId,
-      },
+      data: { ...data, tourOperatorId },
       include: HOTEL_INCLUDE,
     });
   }
@@ -123,10 +120,7 @@ export class PrismaHotelRepository implements HotelRepository {
     data: CreateAgeCategoryDto,
   ): Promise<AgeCategory> {
     return this.prisma.ageCategory.create({
-      data: {
-        ...data,
-        hotelId,
-      },
+      data: { ...data, hotelId },
     });
   }
 
@@ -136,16 +130,12 @@ export class PrismaHotelRepository implements HotelRepository {
   ): Promise<AgeCategory> {
     return this.prisma.ageCategory.update({
       where: { id },
-      data: {
-        ...data,
-      },
+      data: { ...data },
     });
   }
 
   async findAgeCategoryById(id: string): Promise<AgeCategory | null> {
-    return this.prisma.ageCategory.findUnique({
-      where: { id },
-    });
+    return this.prisma.ageCategory.findUnique({ where: { id } });
   }
 
   async findOverlappingAgeCategory(
@@ -165,9 +155,7 @@ export class PrismaHotelRepository implements HotelRepository {
 
   async deleteAgeCategory(id: string): Promise<RepositoryResult> {
     try {
-      await this.prisma.ageCategory.delete({
-        where: { id },
-      });
+      await this.prisma.ageCategory.delete({ where: { id } });
       return RepositoryResult.DELETED;
     } catch (error) {
       if (error instanceof Prisma.PrismaClientKnownRequestError) {
@@ -179,9 +167,7 @@ export class PrismaHotelRepository implements HotelRepository {
   }
 
   async findAllRoomTypes(hotelId: string): Promise<RoomType[]> {
-    return this.prisma.roomType.findMany({
-      where: { hotelId },
-    });
+    return this.prisma.roomType.findMany({ where: { hotelId } });
   }
 
   async findRoomTypeByCode(
@@ -207,27 +193,20 @@ export class PrismaHotelRepository implements HotelRepository {
     data: CreateRoomTypeDto,
   ): Promise<RoomType> {
     return this.prisma.roomType.create({
-      data: {
-        ...data,
-        hotelId,
-      },
+      data: { ...data, hotelId },
     });
   }
 
   async updateRoomType(id: string, data: UpdateRoomTypeDto): Promise<RoomType> {
     return this.prisma.roomType.update({
       where: { id },
-      data: {
-        ...data,
-      },
+      data: { ...data },
     });
   }
 
   async deleteRoomType(id: string): Promise<RepositoryResult> {
     try {
-      await this.prisma.roomType.delete({
-        where: { id },
-      });
+      await this.prisma.roomType.delete({ where: { id } });
       return RepositoryResult.DELETED;
     } catch (error) {
       if (error instanceof Prisma.PrismaClientKnownRequestError) {
