7b05914e41b68f4a99b446af8cb8f13a8ffadaae
tokimanana <tokimananasarobidy@gmail.com>
2026-05-12 19:07:15 +0300

    feat(meal-plans): add create and update DTOs

--- apps/backend/src/meal-plans/dto/create-meal-plan.dto.ts
+++ apps/backend/src/meal-plans/dto/create-meal-plan.dto.ts
@@ -1,1 +1,15 @@
-export class CreateMealPlanDto {}
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateMealPlanDto {
+  @IsString()
+  @IsNotEmpty()
+  code: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+}
