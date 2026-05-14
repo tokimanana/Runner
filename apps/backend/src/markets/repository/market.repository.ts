e281f4684ae95efe600f07b07f6288b586a60907
tokimanana <tokimananasarobidy@gmail.com>
2026-05-14 23:51:39 +0300

    feat(markets): add MarketRepository abstract class

---
+++ apps/backend/src/markets/repository/market.repository.ts
@@ -0,0 +1,33 @@
+import { RepositoryResult } from '@backend/common/repository.types';
+import { Market } from '@prisma/client';
+import { PaginatedResult } from '@runner/shared/types';
+import { CreateMarketDto } from '../dto/create-market.dto';
+import { UpdateMarketDto } from '../dto/update-market.dto';
+
+export abstract class MarketRepository {
+  abstract findAll(
+    tourOperatorId: string,
+    query?: {
+      limit?: number;
+      offset?: number;
+    },
+  ): Promise<PaginatedResult<Market>>;
+
+  abstract findOne(id: string, tourOperatorId: string): Promise<Market | null>;
+
+  abstract create(
+    dto: CreateMarketDto,
+    tourOperatorId: string,
+  ): Promise<Market>;
+
+  abstract update(
+    id: string,
+    dto: UpdateMarketDto,
+    tourOperatorId: string,
+  ): Promise<Market>;
+
+  abstract remove(
+    id: string,
+    tourOperatorId: string,
+  ): Promise<RepositoryResult>;
+}
