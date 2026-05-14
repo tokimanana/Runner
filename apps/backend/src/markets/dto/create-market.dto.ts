eca0d7365c655819428a71ceac97bfd258bb4b72
tokimanana <tokimananasarobidy@gmail.com>
2026-05-14 23:42:23 +0300

    feat(markets): add Market shared types

--- apps/backend/src/markets/dto/create-market.dto.ts
+++ apps/backend/src/markets/dto/create-market.dto.ts
@@ -1,1 +1,11 @@
-export class CreateMarketDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateMarketDto {
+  @IsString()
+  @IsNotEmpty()
+  code: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+}
