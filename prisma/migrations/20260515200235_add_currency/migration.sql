b803b97da8ed2867ea3ac29922fb3d96d39aa61c
tokimanana <tokimananasarobidy@gmail.com>
2026-05-15 23:03:38 +0300

    feat(currencies): add new migration

---
+++ prisma/migrations/20260515200235_add_currency/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Currency" (
+    "id" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "symbol" TEXT NOT NULL,
+
+    CONSTRAINT "Currency_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Currency_code_key" ON "Currency"("code");
