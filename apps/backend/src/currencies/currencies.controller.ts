b4b0cd8bc2fee447196df4c14d35aa41e905663f
tokimanana <tokimananasarobidy@gmail.com>
2026-05-18 23:19:01 +0300

    feat(currencies): add CurrenciesController

--- apps/backend/src/currencies/currencies.controller.ts
+++ apps/backend/src/currencies/currencies.controller.ts
@@ -1,13 +1,31 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CurrenciesService } from './currencies.service';
 import { CreateCurrencyDto } from './dto/create-currency.dto';
 import { UpdateCurrencyDto } from './dto/update-currency.dto';
+import { Roles } from '@backend/auth/decorators/roles.decorator';
+import { JwtAuthGuard } from '@backend/auth/guards/jwt-auth.guard';
+import { RolesGuard } from '@backend/auth/guards/roles.guard';
+import { UserRole } from '@prisma/client';
 
 @Controller('currencies')
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(UserRole.ADMIN, UserRole.MANAGER)
 export class CurrenciesController {
   constructor(private readonly currenciesService: CurrenciesService) {}
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() createCurrencyDto: CreateCurrencyDto) {
     return this.currenciesService.create(createCurrencyDto);
   }
@@ -19,16 +37,20 @@ export class CurrenciesController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.currenciesService.findOne(+id);
+    return this.currenciesService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCurrencyDto: UpdateCurrencyDto) {
-    return this.currenciesService.update(+id, updateCurrencyDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateCurrencyDto: UpdateCurrencyDto,
+  ) {
+    return this.currenciesService.update(id, updateCurrencyDto);
   }
 
   @Delete(':id')
+  @HttpCode(204)
   remove(@Param('id') id: string) {
-    return this.currenciesService.remove(+id);
+    return this.currenciesService.remove(id);
   }
 }
