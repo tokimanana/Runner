bf503cf345e2e41b0ad535e4fa664e4079a577c7
tokimanana <tokimananasarobidy@gmail.com>
2026-05-18 22:19:32 +0300

    feat(currencies): add create and update DTOs

---
+++ apps/backend/src/currencies/dto/create-currency.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateCurrencyDto {
+  @IsString()
+  @IsNotEmpty()
+  code: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  symbol: string;
+}
