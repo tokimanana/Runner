8c6eabcbffb3fcb0b410537b3ea638a65fa45006
tokimanana <tokimananasarobidy@gmail.com>
2026-05-21 23:16:19 +0300

    feat(supplements): generated components

---
+++ apps/frontend/src/app/features/management/supplements/supplements-form/supplements-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-supplements-form',
+  imports: [],
+  templateUrl: './supplements-form.component.html',
+  styleUrl: './supplements-form.component.scss'
+})
+export class SupplementsFormComponent {
+
+}
---
+++ apps/frontend/src/app/features/management/supplements/supplements-list/supplements-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-supplements-list',
+  imports: [],
+  templateUrl: './supplements-list.component.html',
+  styleUrl: './supplements-list.component.scss'
+})
+export class SupplementsListComponent {
+
+}
