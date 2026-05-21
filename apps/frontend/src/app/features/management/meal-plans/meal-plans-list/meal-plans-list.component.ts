657ec6a68fdd34bfbe804ba140fc114d373704d4
tokimanana <tokimananasarobidy@gmail.com>
2026-05-21 23:12:02 +0300

    feat(meal-plans): generated components

---
+++ apps/frontend/src/app/features/management/meal-plans/meal-plans-list/meal-plans-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meal-plans-list',
+  imports: [],
+  templateUrl: './meal-plans-list.component.html',
+  styleUrl: './meal-plans-list.component.scss'
+})
+export class MealPlansListComponent {
+
+}
