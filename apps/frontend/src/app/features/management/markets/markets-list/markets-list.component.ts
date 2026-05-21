89a0a83a31a05f3583e07bd6e8fabc18aa53b576
tokimanana <tokimananasarobidy@gmail.com>
2026-05-21 23:12:43 +0300

    feat(markets): generated components

---
+++ apps/frontend/src/app/features/management/markets/markets-list/markets-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-markets-list',
+  imports: [],
+  templateUrl: './markets-list.component.html',
+  styleUrl: './markets-list.component.scss'
+})
+export class MarketsListComponent {
+
+}
