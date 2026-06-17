d1c94a68a931c831a14a0f0f731207aec4fef4fe
tokimanana <tokimananasarobidy@gmail.com>
2026-06-17 22:12:06 +0300

    feat(contracts): implement contracts CRUD endpoints

--- apps/backend/src/contracts/contracts.controller.ts
+++ apps/backend/src/contracts/contracts.controller.ts
@@ -1,34 +1,77 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Roles } from '@backend/auth/decorators/roles.decorator';
+import { JwtAuthGuard } from '@backend/auth/guards/jwt-auth.guard';
+import { RolesGuard } from '@backend/auth/guards/roles.guard';
+import { RequestWithUser } from '@backend/auth/types/jwt-user.type';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { UserRole } from '@prisma/client';
 import { ContractsService } from './contracts.service';
+import { ContractQuery } from './contracts.types';
 import { CreateContractDto } from './dto/create-contract.dto';
 import { UpdateContractDto } from './dto/update-contract.dto';
 
 @Controller('contracts')
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(UserRole.ADMIN, UserRole.MANAGER)
 export class ContractsController {
   constructor(private readonly contractsService: ContractsService) {}
 
   @Post()
-  create(@Body() createContractDto: CreateContractDto) {
-    return this.contractsService.create(createContractDto);
+  @HttpCode(HttpStatus.CREATED)
+  create(
+    @Body() createContractDto: CreateContractDto,
+    @Req() req: RequestWithUser,
+  ) {
+    const tourOperatorId = req.user.tourOperatorId;
+    return this.contractsService.create(createContractDto, tourOperatorId);
   }
 
   @Get()
-  findAll() {
-    return this.contractsService.findAll();
+  findAll(
+    @Req() req: RequestWithUser,
+    @Query() { limit, offset, hotelId, marketId }: ContractQuery,
+  ) {
+    const tourOperatorId = req.user.tourOperatorId;
+    return this.contractsService.findAll(tourOperatorId, {
+      limit: limit ? Number(limit) : undefined,
+      offset: offset ? Number(offset) : undefined,
+      hotelId,
+      marketId,
+    });
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.contractsService.findOne(+id);
+  findOne(@Param('id') id: string, @Req() req: RequestWithUser) {
+    const tourOperatorId = req.user.tourOperatorId;
+    return this.contractsService.findOne(id, tourOperatorId);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateContractDto: UpdateContractDto) {
-    return this.contractsService.update(+id, updateContractDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateContractDto: UpdateContractDto,
+    @Req() req: RequestWithUser,
+  ) {
+    const tourOperatorId = req.user.tourOperatorId;
+    return this.contractsService.update(id, updateContractDto, tourOperatorId);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.contractsService.remove(+id);
+  @HttpCode(204)
+  remove(@Param('id') id: string, @Req() req: RequestWithUser) {
+    const tourOperatorId = req.user.tourOperatorId;
+    return this.contractsService.remove(id, tourOperatorId);
   }
 }
